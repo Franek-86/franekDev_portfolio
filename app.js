1caf2667848567f25f7e0f61419fd0d244370d4c
Franek-86 <fraleccese86@gmail.com>
2024-07-04 14:18:53 +0200

    save

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { data } from "./data.js";
+console.log(data);
 const closeBtn = document.querySelector(".close-btn");
 const navBtn = document.querySelector(".nav-btn");
 const sidebar = document.querySelector(".sidebar");
@@ -17,4 +19,154 @@ window.addEventListener("scroll", () => {
   } else {
     navBar.classList.remove("fixed-nav");
   }
+});
+
+// language
+const langEl = document.querySelector(".langWrap");
+const link = document.querySelectorAll("a");
+const titleEl = document.querySelector(".bannerTitle");
+const descrEl = document.querySelector(".bannerDescription");
+const aboutTitle = document.querySelector(".aboutTitle");
+const aboutDescription1 = document.querySelector(".aboutDescription1");
+const aboutDescription2 = document.querySelector(".aboutDescription2");
+const webDevelopmentText = document.querySelector(".webDevelopmentText");
+const webDesignText = document.querySelector(".webDesignText");
+const appDesignText = document.querySelector(".appDesignText");
+const latestWorksTitle = document.querySelector(".latestWorksTitle");
+const latestWorksText = document.querySelector(".latestWorksText");
+const touchTitle = document.querySelector(".touchTitle");
+const touchText = document.querySelector(".touchText");
+const touchBtn = document.querySelector(".touchBtn");
+const bannerBtn = document.querySelector(".bannerBtn");
+const navHome = document.querySelector(".navHome");
+const navAbout = document.querySelector(".navAbout");
+const navContact = document.querySelector(".navContact");
+const navProjects = document.querySelector(".navProjects");
+const sidebarHome = document.querySelector(".sidebarHome");
+const sidebarAbout = document.querySelector(".sidebarAbout");
+const sidebarContact = document.querySelector(".sidebarContact");
+const sidebarProjects = document.querySelector(".sidebarProjects");
+const projectPage = document.querySelector(".projectPage");
+const aboutMeBtn = document.querySelector(".aboutMeBtn");
+const singleAboutTitle = document.querySelector(".singleAboutTitle");
+const singleAboutText = document.querySelector(".singleAboutText");
+const singleContactTitle = document.querySelector(".singleContactTitle");
+const singleContactText1 = document.querySelector(".singleContactText1");
+const singleContactText2 = document.querySelector(".singleContactText2");
+const singleProjectsTitle = document.querySelector(".singleProjectsTitle");
+
+const translate = (attr) => {
+  if (titleEl) {
+    titleEl.textContent = data[attr].bannerTitle;
+  }
+  if (descrEl) {
+    descrEl.textContent = data[attr].bannerDescription;
+  }
+  if (aboutTitle) {
+    aboutTitle.textContent = data[attr].aboutTitle;
+  }
+  if (aboutDescription1) {
+    aboutDescription1.textContent = data[attr].aboutDescription1;
+  }
+  if (aboutDescription2) {
+    aboutDescription2.textContent = data[attr].aboutDescription2;
+  }
+  if (webDevelopmentText) {
+    webDevelopmentText.textContent = data[attr].webDevelopmentText;
+  }
+  if (webDesignText) {
+    webDesignText.textContent = data[attr].webDesignText;
+  }
+  if (appDesignText) {
+    appDesignText.textContent = data[attr].appDesignText;
+  }
+  if (latestWorksTitle) {
+    latestWorksTitle.textContent = data[attr].latestWorksTitle;
+  }
+  if (latestWorksText) {
+    latestWorksText.textContent = data[attr].latestWorksText;
+  }
+  if (touchTitle) {
+    touchTitle.textContent = data[attr].touchTitle;
+  }
+  if (touchText) {
+    touchText.textContent = data[attr].touchText;
+  }
+  if (touchBtn) {
+    touchBtn.textContent = data[attr].touchBtn;
+  }
+  if (bannerBtn) {
+    bannerBtn.textContent = data[attr].bannerBtn;
+  }
+  if (navHome) {
+    navHome.textContent = data[attr].navHome;
+  }
+  if (navAbout) {
+    navAbout.textContent = data[attr].navAbout;
+  }
+  if (navContact) {
+    navContact.textContent = data[attr].navContact;
+  }
+  if (navProjects) {
+    navProjects.textContent = data[attr].navProjects;
+  }
+  if (sidebarHome) {
+    sidebarHome.textContent = data[attr].sidebarHome;
+  }
+  if (sidebarAbout) {
+    sidebarAbout.textContent = data[attr].sidebarAbout;
+  }
+  if (sidebarContact) {
+    sidebarContact.textContent = data[attr].sidebarContact;
+  }
+  if (sidebarProjects) {
+    sidebarProjects.textContent = data[attr].sidebarProjects;
+  }
+  if (aboutMeBtn) {
+    aboutMeBtn.textContent = data[attr].aboutMeBtn;
+  }
+  if (projectPage) {
+    projectPage.textContent = data[attr].projectPage;
+  }
+  if (singleAboutTitle) {
+    singleAboutTitle.textContent = data[attr].singleAboutTitle;
+  }
+  if (singleAboutText) {
+    singleAboutText.textContent = data[attr].singleAboutText;
+  }
+  if (singleContactTitle) {
+    singleContactTitle.textContent = data[attr].singleContactTitle;
+  }
+  if (singleContactText1) {
+    singleContactText1.textContent = data[attr].singleContactText1;
+  }
+  if (singleContactText2) {
+    singleContactText2.textContent = data[attr].singleContactText2;
+  }
+  if (singleProjectsTitle) {
+    singleProjectsTitle.textContent = data[attr].singleProjectsTitle;
+  }
+};
+window.addEventListener("DOMContentLoaded", () => {
+  const test = localStorage.getItem("lang");
+  console.log("test", test);
+  if (test) {
+    translate(test);
+
+    console.log(langEl.querySelector(".active"));
+    langEl.querySelector(".active").classList.remove("active");
+    console.log("test2", langEl.querySelector('[language="' + test + '"]'));
+    langEl.querySelector('[language="' + test + '"]').classList.add("active");
+  }
+
+  link.forEach((el) => {
+    el.addEventListener("click", () => {
+      langEl.querySelector(".active").classList.remove("active");
+      el.classList.add("active");
+
+      let attr = el.getAttribute("language");
+      translate(attr);
+      localStorage.setItem("lang", attr);
+    });
+  });
 });
---
+++ data.js
@@ -0,0 +1,90 @@
+export const data = {
+  english: {
+    navHome: "Home",
+    navAbout: "About",
+    navContact: "Contact",
+    navProjects: "Projects",
+    sidebarHome: "Home",
+    sidebarAbout: "About",
+    sidebarContact: "Contact",
+    sidebarProjects: "Projects",
+    bannerTitle: "I'm Franek",
+    bannerDescription: "Freelance Web Developer",
+    bannerBtn: "HIRE ME",
+    aboutTitle: "About",
+    aboutDescription1:
+      " Hi all! I am Francesco Leccese but you can call me Franek. I am a Front-End Developer based in Italy with Marketing and Communication studies and six years of experience providing IT services for customers geographically dispersed all over the world and one year of experience in web development.",
+    aboutDescription2:
+      " In have a passion for technology and visual arts, especially photography and design.",
+    aboutMeBtn: "ABOUT ME",
+    webDevelopmentText:
+      "Create, build, and maintain your websites and web applications.",
+    webDesignText:
+      "Focus on aesthetic factors like layout, user interface and other visual imagery in order to make your website more visually appealing and easy to use.",
+    appDesignText:
+      "Deep care of the overall style of your app combined with the focus on the actual functionality and usability of your application.",
+    latestWorksTitle: "Latest Works",
+    latestWorksText:
+      "Here below you can find some of my latest works and many more are stored in my ",
+    projectPage: "projects page",
+    touchTitle: "let's get in touch",
+    touchText:
+      "Want to work together or have any questions? I look forward to here from you.",
+    touchBtn: "CONTACT ME",
+    singleAboutTitle: "about me",
+    singleAboutText:
+      "Hello, I am Francesco Leccese and I am a Front-End developer. I live and work between Italy and Poland. I spend most of my day experimenting with HTML, CSS, JavaScript and its React and Vue frameworks. I enjoy coding and the challenge of learning something new everyday. You can also find me on Linkedin",
+    singleContactTitle: "contact",
+    singleContactText1:
+      "   If you are looking to get ahold of me, you can send me an email at",
+    singleContactText2: ". You can also reach me on Linkedin at",
+    singleProjectsTitle: "my projects",
+  },
+
+  italian: {
+    navHome: "Home",
+    navAbout: "Su di me",
+    navContact: "Contatti",
+    navProjects: "Progetti",
+    sidebarHome: "Home",
+    sidebarAbout: "Su di me",
+    sidebarContact: "Contatti",
+    sidebarProjects: "Progetti",
+    bannerTitle: "Sono Franek",
+    bannerDescription: "Programmatore Front-End Freelance",
+    bannerBtn: "ASSUMIMI",
+    aboutTitle: "su di me",
+    aboutDescription1:
+      "Ciao a tutti! Sono Francesco Leccese ma potete chiamarmi Franek. Sono uno sviluppatore front-end con sede in Italia con studi di marketing e comunicazione e sei anni di esperienza nella fornitura di servizi IT per clienti geograficamente dispersi in tutto il mondo e un anno di esperienza nello sviluppo web.Ho una passione per la tecnologia e le arti visive, in particolare la fotografia e il design.",
+    aboutDescription2:
+      "Ho una passione per la tecnologia e le arti visive, in particolare la fotografia e il design.",
+    aboutMeBtn: "SU DI ME",
+    webDevelopmentText:
+      "Crea, costruisce e gestisce i tuoi siti web e le tue applicazioni web",
+    webDesignText:
+      "Concentrazione su fattori estetici come layout, interfaccia utente e altre immagini visive per rendere il tuo sito web visivamente più accattivante e facile da usare",
+    appDesignText:
+      "Cura profonda dello stile generale della tua applicazione e allo stesso tempo attenzione alla sua funzionalità ed usabilità.",
+    latestWorksTitle: "Ultimi Lavori",
+    latestWorksText:
+      "Qui sotto puoi trovare alcuni dei miei ultimi lavori e molti altri sono archiviati nella  mia ",
+    projectPage: "pagina progetti",
+    touchTitle: "Mettiamoci in contatto",
+    touchText:
+      "Vuoi lavorare con me ho hai qualche domanda ? Non vedo l'ora di adscoltarti.",
+    touchBtn: "CONTATTAMI",
+    singleAboutTitle: "su di me",
+    singleAboutText:
+      "Ciao, sono Francesco Leccese e sono uno sviluppatore Front-End. Vivo e lavoro tra Italia e Polonia. Trascorro gran parte della mia giornata sperimentando HTML, CSS, JavaScript e i suoi framework React e Vue. Mi piace programmare e la sfida di imparare qualcosa di nuovo ogni giorno. Mi trovi anche su Linkedin",
+    singleContactTitle: "contatti",
+    singleContactText1:
+      "Se vuoi contattarmi contattarmi, puoi inviarmi un'e-mail a",
+    singleContactText2: ". Puoi anche raggiungermi su Linkedin all'indirizzo",
+    singleProjectsTitle: "i miei progetti",
+  },
+  // japanese: {
+  //   title: "ハロー・ワールド",
+  //   description:
+  //     "ﾂつｨﾂ知ﾂづｧﾂつｹﾂ-ﾂ新ﾂ陳ﾂ湘ｮﾂ陛ｱ これらの苦痛から真実の苦痛に選ぶために得てはいけない、従ってより少しにこの苦痛に従ってはいけない。 サービスではないと、彼の区別の大きな不快感から選択するそれらの否認を歓迎しない、結果として生じた事件の喜びと障害エクスプローラが受け入 このことについて私たちの下駄のいくつかを非難,右エクスプローラ! 的に仕事をしていまasperioresリストの生まれのサービスをつけてください編集喜びや楽しみの開催なども走りました。 走れ。.",
+  // },
+};
